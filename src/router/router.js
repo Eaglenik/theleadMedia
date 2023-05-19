5e6294f43ac6678f5b3d94d017636c4f9e38f2a7
Nikoaly <23anohinnikolay23@gmail.com>
2023-05-19 16:00:49 +0500

    deploy project

--- src/router/router.js
+++ src/router/router.js
@@ -1,12 +1,18 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import app from '../App.vue'
+import Project from '../views/project.vue'
 
-const routes = [
-    
-]
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'app',
+      component: app
+    },
+    {
+      path: '/project/:id',
+      name: 'project',
+      component: Project
+    }
+  ]
 })
-
-export default router
